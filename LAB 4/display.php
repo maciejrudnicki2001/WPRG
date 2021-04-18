78b9f13783abab1c82d96fc8b3853ba9b9cbc699
maciejrudnicki2001 <s22983@pjwstk.edu.pl>
2021-04-18 18:24:26 +0200

    ex1 lab4

---
+++ LAB 4/display.php
@@ -0,0 +1,13 @@
+<form action="#" method="post" >
+    Number A: <input type="number" name="a"><br> </br>
+    Number B: <input type="number" name="b"><br></br>
+    Operation:
+    </br>
+        Sum:<input type="radio" name="wyb" value="Sum"></br>
+		Difference:<input type="radio" name="wyb" value="Difference"></br>
+        Multiplication:<input type="radio" name="wyb" value="Multiplication"></br>
+        Division: <input type="radio" name="wyb" value="Division"></br>
+    </br>
+    <button type="submit" name="button" formmethod="post">Send</button>
+</form>
+</form>
