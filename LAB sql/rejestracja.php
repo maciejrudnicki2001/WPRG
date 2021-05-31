c04b408d7b5eef688ef326517b05fb3c9f29621f
maciejrudnicki2001 <75856604+maciejrudnicki2001@users.noreply.github.com>
2021-05-31 16:31:17 +0200

    Update rejestracja.php

--- LAB sql/rejestracja.php
+++ LAB sql/rejestracja.php
@@ -3,7 +3,7 @@
     if (isset($_POST['rejestruj']))
     {	
 		$con = mysqli_connect("localhost","postgres","nie_podam_sory","postgres","5432");
-		$quer = mysqli_query($con,"SELECT * FROM uzytkownicy") or die("Nie połączyło mordo");
+		$quer = mysqli_query($con,"SELECT * FROM uzytkownicy") or die("Nie połączyło");
 	}
 
 	while ($row = mysqli_fetch_assoc($result)) {
